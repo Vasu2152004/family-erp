81e0c0a220f4cbc38a90b83de76e884fe7bf0fd7
vasu <vkkalavadiya21@gmail.com>
2026-02-03 16:59:01 +0530

    fixing family creation error

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Family;
+use App\Services\FamilyMemberService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
